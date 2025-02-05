03de6862a35bfb2c41a65343dfd45201a6d1e8a4
Rakhi Sharma <144309006+Sharma-Rakhi1@users.noreply.github.com>
2025-02-05 12:49:05 +0530

    Create Java_Inheritance.java
    Added DAY-5 solution

---
+++ DAY-5/Java_Inheritance.java
@@ -0,0 +1,14 @@
+//User function Template for Java
+
+class cls2 extends cls1
+{
+    void mul(int p,int q)
+    {
+        System.out.println(p*q);
+    }
+    void task(int p,int q)
+    {
+        //Add your code here.
+        System.out.println(p*p + q*q);
+    }
+}
