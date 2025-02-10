140a69ab7d4e6f640adb56a526c28c96254853c5
Rakhi Sharma <144309006+Sharma-Rakhi1@users.noreply.github.com>
2025-02-11 00:05:39 +0530

    Update Abstractkeyword.java adding code

--- DAY-10/Abstractkeyword.java
+++ DAY-10/Abstractkeyword.java
@@ -1,1 +1,9 @@
+//User function Template for Java
 
+class B extends A 
+{
+    void m1(int p,int q) {
+       // Add your code here.
+      prod = p* q; //where prod is an attribute of parent class
+    }
+}
